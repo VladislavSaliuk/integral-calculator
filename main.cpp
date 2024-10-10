3d765fb338fdee89f85fd65d8841147aa13fc957
Vladislav Saliuk <seattlemusic345@gmail.com>
2024-10-10 10:19:35 +0300

    Implementing integral calculator

--- main.cpp
+++ main.cpp
@@ -1,6 +1,79 @@
 #include <iostream>
+#include <cmath>
+#include <pthread.h>
+#include <vector>
+#include <ctime>
+
+using namespace std;
+
+struct ThreadData {
+    double left;
+    double right;
+    int steps;
+    double result;
+};
+
+double function(double x) {
+    return x * x * sin(x) + log(x + 1) + exp(-x);
+}
+
+void* calculate_integral(void* arg) {
+    ThreadData* data = (ThreadData*) arg;
+    double step_size = (data->right - data->left) / data->steps;
+    double sum = 0.0;
+
+    for (int i = 1; i <= data->steps; ++i) {
+        double x = data->left + i * step_size;
+        sum += function(x) * step_size;
+    }
+
+    data->result = sum;
+    pthread_exit(NULL);
+}
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    double left, right;
+    int num_threads, steps;
+
+    cout << "Enter the left boundary of integration: ";
+    cin >> left;
+
+    cout << "Enter the right boundary of integration: ";
+    cin >> right;
+
+    cout << "Enter the number of threads: ";
+    cin >> num_threads;
+
+    cout << "Enter the number of integration steps: ";
+    cin >> steps;
+
+    pthread_t threads[num_threads];
+    vector<ThreadData> thread_data(num_threads);
+
+    double interval_length = (right - left) / num_threads;
+    int steps_per_thread = steps / num_threads;
+
+    clock_t start = clock();
+
+    for (int i = 0; i < num_threads; ++i) {
+        thread_data[i].left = left + i * interval_length;
+        thread_data[i].right = thread_data[i].left + interval_length;
+        thread_data[i].steps = steps_per_thread;
+        pthread_create(&threads[i], NULL, calculate_integral, (void*)&thread_data[i]);
+    }
+
+    double total_sum = 0.0;
+
+    for (int i = 0; i < num_threads; ++i) {
+        pthread_join(threads[i], NULL);
+        total_sum += thread_data[i].result;
+    }
+
+    clock_t end = clock();
+    double time_spent = (double)(end - start) / CLOCKS_PER_SEC;
+
+    cout << "The result of the integral: " << total_sum << endl;
+    cout << "Calculation time: " << time_spent << " seconds" << endl;
+
     return 0;
 }
